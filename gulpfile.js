1813ecf93657da76cb7b9bf91698d90d11da2fe8
Stephen Poole <stephen.poole@redlioncanada.com>
2016-03-23 10:37:05 -0400

    Added version to build filenames

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require('gulp'),
 	babel = require('gulp-babel'),
 	minify = require('gulp-minify'),
-	jshint = require('gulp-jshint');
+	jshint = require('gulp-jshint'),
+	package = require('./package.json');
 
 gulp.task('js', function() {
+	var version = package.version;
 	return gulp.src('src/*.js')
 		.pipe(babel())
 		.pipe(jshint())
-		.pipe(minify())
+		.pipe(minify({ext: {min: "-"+version+".min.js", src: "-"+version+".js"}}))
 		.pipe(gulp.dest('build'));
 });
 
-gulp.task('default', function() {
+gulp.task('default', ['js'], function() {
 	gulp.watch('src/*.js', ['js']);
 });
