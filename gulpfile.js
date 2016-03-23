338091b558d03e06ea13789bbe0240507438d065
Stephen Poole <stephen.poole@redlioncanada.com>
2016-03-23 10:40:14 -0400

    Clean build folder first on every compile

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ var gulp = require('gulp'),
 	babel = require('gulp-babel'),
 	minify = require('gulp-minify'),
 	jshint = require('gulp-jshint'),
+	del = require('del'),
 	package = require('./package.json');
 
-gulp.task('js', function() {
+gulp.task('js', ['clean'], function() {
 	var version = package.version;
 	return gulp.src('src/*.js')
 		.pipe(babel())
@@ -12,6 +13,10 @@ gulp.task('js', function() {
 		.pipe(minify({ext: {min: "-"+version+".min.js", src: "-"+version+".js"}}))
 		.pipe(gulp.dest('build'));
 });
+
+gulp.task('clean', function() {
+	return del(['./build'])
+})
 
 gulp.task('default', ['js'], function() {
 	gulp.watch('src/*.js', ['js']);
